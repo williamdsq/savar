43c24e01c5c28ffc64393840239c4f9978d3e8b2
williamdsq <williamdsq@gmail.com>
2025-04-03 20:48:51 -0300

    Update camera_server.py

--- camera_server.py
+++ camera_server.py
@@ -2,10 +2,7 @@ from flask import Flask, Response
 import cv2
 
 app = Flask(__name__)
-
-# Endereço RTSP da sua câmera Intelbras
-rtsp_url = "rtsp://admin:2i7xw458@192.168.0.105:554/cam/realmonitor?channel=1&subtype=0"
-cap = cv2.VideoCapture(rtsp_url)
+cap = cv2.VideoCapture("rtsp://admin:2i7xw458@192.168.0.105:554/cam/realmonitor?channel=1&subtype=0")
 
 def gen_frames():
     while True:
@@ -14,16 +11,11 @@ def gen_frames():
             break
         _, buffer = cv2.imencode('.jpg', frame)
         frame = buffer.tobytes()
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+        yield (b'--frame\r\nContent-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 @app.route('/video_feed')
 def video_feed():
     return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
-
-@app.route('/')
-def index():
-    return "<h1>Servidor de câmera Intelbras rodando!</h1><img src='/video_feed'>"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
